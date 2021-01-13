d650724abe519e33af6cc91cdcaf00fe42e4e3a8
Bantexx <defotrue@gmail.com>
2021-01-13 19:05:49 +0300

    add_labs

---
+++ TestingLabs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
